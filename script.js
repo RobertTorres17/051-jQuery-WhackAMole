c49ddaacbca4d0727c5ea19d5ef9c95114a19060
RobertTorres17 <49485666+RobertTorres17@users.noreply.github.com>
2019-04-10 09:43:12 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$("#mole1").click(function(){
+    $("#mole1").hide();
+});
+    
+$("#moleTwo").click(function(){
+    $("#moleTwo").hide();
+}); 
+    
+$("#thirdMole").click(function(){
+    $("#thirdMole").hide();
+});
+    
+$("#mole").click(function(){
+    $("INCORRECT").show();
+});
